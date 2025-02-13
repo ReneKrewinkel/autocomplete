16b3a86e4d3657090911da95e788ec20fa7648e4
René Krewinkel <rene@studiokrijst.com>
2025-02-13 11:47:41 +0100

    Step 6: list

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import React from 'react'
 import './style/main.css'
 
 import { Form, List } from './components/molecules'
+import { useFetch } from './hooks'
+import { config } from './config'
 
 
 function App() {
 
+  const { data, isLoaded, error } = useFetch(config.api)
+
   return (
     <div className="App">
         <Form />
-        <List />
+      {  isLoaded  &&  <List data={data}/> }
+
+      { error && <div>Error: { JSON.stringify(error) }</div> }
+
     </div>
   )
 }
--- src/components/molecules/List.tsx
+++ src/components/molecules/List.tsx
@@ -1,7 +1,24 @@
-const List = () => {
+interface ListProps {
+  data: any | null
+}
+
+const List = ({ data }: ListProps) => {
   return (
     <div>
-      <h1>List</h1>
+      {
+        data.map((item: any, index: number) => {
+          return(
+             <div className={"ListRow"} key={index}>
+               <div>{item.name}</div>
+               <div>{item.email}</div>
+               <div>{item.address.street}</div>
+               <div>{item.address.suite}</div>
+               <div>{item.address.city}</div>
+             </div>
+          )
+        })
+
+      }
     </div>
   )
 }
